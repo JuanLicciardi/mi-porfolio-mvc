6a5043ffc2b703e2c261b807048c08e15f2a79c9
JuanLicciardi <juanelicciardi@gmail.com>
2022-08-06 09:03:47 -0300

    Micro desafios Paso5 - Etapa I Finalizado

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ const port= 3030;
 
 const app=express();
 
-
+app.use(express.static('public'));
 app.listen(port, ()=> console.log('Server running in http://localhost:'+port));
+
+app.get('/', (req,res)=> res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/about', (req,res)=> res.sendFile(path.resolve(__dirname,'views','about.html')));
+
